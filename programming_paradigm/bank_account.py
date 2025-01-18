754d7710abd9189af98846d8c26e8fb70fe7b1ba
nati1322 <natnaelterefe44@gmail.com>
2025-01-18 11:13:27 +0300

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,7 +10,6 @@ class BankAccount:
             self.account_balance -= amount
             return True
         else:
-            print("Insufficient funds.")
             return False
 
     def display_balance(self):
