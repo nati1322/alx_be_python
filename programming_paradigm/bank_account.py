b5c92b82ea5c3d4ebeffdc695b285e66255b8cd4
nati1322 <natnaelterefe44@gmail.com>
2025-01-18 11:12:22 +0300

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,6 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
