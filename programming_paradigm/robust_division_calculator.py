a7dcd8f588ce74e62350fe735c645195b962de56
nati1322 <natnaelterefe44@gmail.com>
2025-01-18 12:52:25 +0300

    Robust Division Calculator with Command Line Arguments

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,14 @@
+class SimpleCalculator:
+    def addition(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        if b == 0:
+            return None
+        return a / b
